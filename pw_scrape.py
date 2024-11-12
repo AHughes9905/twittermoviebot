2bd60c2c6118b8eaea3d3d05bddc936be79f7fa2
Hughes, Aaron John <ahughes9905@gmail.com>
2024-11-12 16:36:24 -0700

    Corrected not waiting for 250 page by using playwright locator and selectors

--- pw_scrape.py
+++ pw_scrape.py
@@ -7,6 +7,8 @@ def get_top_250_html():
     page = browser.new_page()
     page.goto('https://www.imdb.com/chart/top/', wait_until="domcontentloaded")
     page.is_visible(':text-matches("^250..*")')
+    last_movie = page.locator("#__next > main > div > div.ipc-page-content-container.ipc-page-content-container--center > section > div > div.ipc-page-grid.ipc-page-grid--bias-left > div > ul > li:nth-child(250) > div.ipc-metadata-list-summary-item__c > div > div > div.ipc-title.ipc-title--base.ipc-title--title.ipc-title-link-no-icon.ipc-title--on-textPrimary.sc-a69a4297-2.bqNXEn.cli-title.with-margin > a")
+    last_movie.wait_for()
     html = page.content()
     p.stop()
     return html
