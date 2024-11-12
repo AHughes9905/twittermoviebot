59d85fa9e64cc6d7d211cd155d7d8d9bd869d81d
Hughes, Aaron John <ahughes9905@gmail.com>
2024-11-12 12:42:49 -0700

    Got it working and tweeting, now to clean up and tweak tweet formatting

--- twitterbot.py
+++ twitterbot.py
@@ -5,19 +5,36 @@ def setupAPI():
     f = open("apicodes.txt", 'r')
     keys = f.readlines()
     keys = [key.strip() for key in keys]
+    consumer = keys[0]
+    consumer_secret = keys[1]
+    access = keys[2]
+    access_secret = keys[3]
     f.close()
-
-    auth = tweepy.OAuthHandler(keys[0], keys[1])
-    auth.set_access_token(keys[2], keys[3])
+    
+    auth = tweepy.OAuthHandler(consumer, consumer_secret)
+    auth.set_access_token(access, access_secret,)
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
     return api
 
-def makeTweet(api, message):
-    api.update_status(message)
+def setupClient():
+    f = open("apicodes.txt", 'r')
+    keys = f.readlines()
+    keys = [key.strip() for key in keys]
+    consumer = keys[0]
+    consumer_secret = keys[1]
+    access = keys[2]
+    access_secret = keys[3]
+    f.close()
 
-def replyTweet(api, message, tweet_id):
-    api.update_status(message, in_reply_to_status_id = tweet_id)
+    client = tweepy.Client(consumer_key = consumer, consumer_secret=consumer_secret, access_token=access, access_token_secret=access_secret)
+    return client
+
+def makeTweet(client, message):
+    return client.create_tweet(text=message)
+
+def replyTweet(client, message, tweet_id):
+    return client.create_tweet(text=message, in_reply_to_tweet_id=tweet_id)
 
 
 def getRecentTweetId(api):
@@ -42,3 +59,17 @@ def tweetList(api, tweet_list):
         else:
             replyTweet(api, tweet, current_id)
             current_id = getRecentTweetId(api)
+
+def tweetList2(client, tweet_list):
+    c = 0
+    current_id = ''
+    for tweet in tweet_list:
+        if c == 0:
+
+            response = makeTweet(client, tweet)
+            current_id = response.data['id']
+            c += 1
+
+        else:
+            response = replyTweet(client, tweet, current_id)
+            current_id = response.data['id']
