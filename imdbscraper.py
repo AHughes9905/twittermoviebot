6c2327f576db51bdb17ca584ce4e6782b2456300
Hughes, Aaron John <ahughes9905@gmail.com>
2024-11-14 14:18:55 -0700

    Changed functions to take and return urls instead of soup objects. Also reformatted tweet headers and removed@ing the page the first tweet. @ing is required on replies per twitter api page.

--- imdbscraper.py
+++ imdbscraper.py
@@ -12,13 +12,44 @@ def soupify(url):
     return bs(webpage.content, "lxml")
 
 
+def get250MovieURL(n):
+    html = get_top_250_html()
+    soup = bs(html, "lxml")
+
+    atag = soup.find_all('div', class_ = "ipc-metadata-list-summary-item__c")[n].find('a')
+    movie_page = atag['href']
+    movie_url = "https://www.imdb.com" + movie_page
+    return movie_url
+
+def getReviewsandInfo(url):
+    soup = soupify(url)
+
+    dropdown_pref = '/?ref_=tt_ov_urv&rating=1&sort=featured%2Cdesc'
+    directory = soup.find('a', class_ = 'ipc-link ipc-link--baseAlt ipc-link--touch-target sc-b782214c-2 kqhWjl isReview')['href']
+    review_url = "https://www.imdb.com" + directory.split('?')[0] + dropdown_pref
+
+    info = movieInfo(soup)
+    return review_url, info
+
+
+def getRandomReviewPage(url):
+    soup = soupify(url)
+    reviews = soup.find_all('article', class_ = 'sc-f53ace6f-1 cHwTOl user-review-item')
+    n = random.randrange(0, len(reviews))
+    review_url = reviews[n].find('a', class_ = 'ipc-title-link-wrapper')['href']
+    url = 'https://www.imdb.com' + review_url + '?ref_=tt_urv'
+    print(f'Review URL: {url}')
+    return url
+
+def getReviewFromPage(url):
+    soup = soupify(url)
+    return soup.find(class_ = 'content').find(class_ = 'text show-more__control').text
+
 def getMoviePageSoup(n):
     
     html = get_top_250_html()
     soup = bs(html, "lxml")
 
-    print(n)
-    print(len(soup.find_all('div', class_ = "ipc-metadata-list-summary-item__c")))
     atag = soup.find_all('div', class_ = "ipc-metadata-list-summary-item__c")[n].find('a')
     movie_page = atag['href']
     movie_url = "https://www.imdb.com" + movie_page
@@ -26,12 +57,14 @@ def getMoviePageSoup(n):
 
 
 
-def movieInfo(msoup, n):
-    movieinfo = {'name': '' , 'rating': '', 'ranking': '', 'review': '', 'user_rating': ''}
+def movieInfo(msoup):
+    movieinfo = {'name': '' , 'rating': '', 'ranking': 'NA', 'review': '', 'user_rating': ''}
 
     movieinfo['name'] = msoup.find('h1').string
     movieinfo['rating'] = msoup.find(class_ = 'sc-d541859f-1 imUuxf').string
-    movieinfo['ranking'] = str(n+1)
+    top_rated = msoup.find('a', class_ = 'ipc-link ipc-link--base ipc-link--inherit-color top-rated-link')
+    if top_rated:
+        movieinfo['ranking'] = top_rated.string.split('#')[1]
 
     return movieinfo
 
@@ -39,13 +72,14 @@ def getReviewPage(msoup):
     dropdown_pref = '/?ref_=tt_ov_urv&rating=1&sort=featured%2Cdesc'
     directory = msoup.find('a', class_ = 'ipc-link ipc-link--baseAlt ipc-link--touch-target sc-b782214c-2 kqhWjl isReview')['href']
     review_url = "https://www.imdb.com" + directory.split('?')[0] + dropdown_pref
-    print(review_url)
+    print('List Page-' + review_url)
     return soupify(review_url)
 
 def specificReviewPage(csoup):
-    n = random.randrange(0,80, 4)
-    review_url = csoup.find('a', class_ = 'ipc-title-link-wrapper')['href']
-    print(review_url)
+    
+    reviews = csoup.find_all('article', class_ = 'sc-f53ace6f-1 cHwTOl user-review-item')
+    n = random.randrange(0, len(reviews))
+    review_url = reviews[n].find('a', class_ = 'ipc-title-link-wrapper')['href']
 
     url = 'https://www.imdb.com' + review_url + '?ref_=tt_urv'
     return soupify(url)
