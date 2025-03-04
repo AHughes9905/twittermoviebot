be2f63d8e21b477a57e2b468989c99ce3f53664f
Hughes, Aaron John <ahughes9905@gmail.com>
2025-03-04 16:57:21 -0700

    Cleaned up main, ready for docker implementation

--- savejson.py
+++ savejson.py
@@ -2,7 +2,7 @@ import json
 import pickle
 Q_SIZE = 7
 
-def checkNewReview(rurl, movie):
+def checkNewReview(rurl):
     with open('reviewURLS.json', 'r') as f:
         data = json.load(f)
     if rurl not in data:
@@ -10,13 +10,15 @@ def checkNewReview(rurl, movie):
     return False
 
 def inRecentMovies(movie):
-    with open('past_queue', 'rb') as saved_q:
+    with open('past-queue', 'rb') as saved_q:
         q = pickle.load(saved_q)
     if movie in q:
         return True
     return False
 
 def updateRecentMovies(movie):
+    with open('past-queue', 'wb+') as saved_q:
+        pickle.dump([], saved_q)
     with open('past-queue', 'rb') as saved_q:
         q = pickle.load(saved_q)
     q.append(movie)
@@ -27,8 +29,6 @@ def updateRecentMovies(movie):
     
     return True
         
-    
-
 def addReview(rurl, movie):
     with open('reviewURLS.json', 'r') as f:
         data = json.load(f)
